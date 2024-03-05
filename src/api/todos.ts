e755a42617c80bd9fc982e78897c55cf4f8bde52
박서영 <seokachuu@gmail.com>
2024-03-06 02:46:00 +0900

    Feat: api todos추가

---
+++ src/api/todos.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const SERVER_URL = import.meta.env.VITE_REACT_APP_SERVER_URL;
+
+export const todoClient = axios.create({
+  baseURL: SERVER_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// 목록 가져오기
+export const getTodos = async () => {
+  const { data } = await todoClient.get('/todos');
+  return data;
+};
+
+//추가하기
+export const createTodos = async (item: any) => {
+  const { data } = await todoClient.post('/todos', item);
+  return data;
+};
+
+//삭제하기
+export const deleteTodos = async (id: any) => {
+  await todoClient.delete(`/todos/${id}`);
+  return id;
+};
+
+//수정하기
+export const updateTodos = async (id: any, todos: any) => {
+  await todoClient.patch(`/todos/${id}`, todos);
+  return id;
+};
