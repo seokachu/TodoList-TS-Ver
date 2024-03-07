2b7400f1ac7445b005796b05c73d43695337880f
박서영 <seokachuu@gmail.com>
2024-03-08 02:22:13 +0900

    Fix: Date타입수정

--- src/util/date.ts
+++
@@ -1,8 +0,0 @@
-export const getFormattedDate = (date: string) =>
-  new Date(date).toLocaleDateString('ko-KR', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-  });
